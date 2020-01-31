24c45d023470b84a391352bec22edd5200fa15a5
Teodor G <grigor.teodor@gmail.com>
2020-01-31 20:37:04 +0000

    Update MainCharacterScript.cs

--- Assets/Scripts/ShopScreen/MainCharacterScript.cs
+++ Assets/Scripts/ShopScreen/MainCharacterScript.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainCharacterScript : MonoBehaviour {
 
-    private int isMoving = 0;
+    private int isMoving = 0, isZooming = 0;
     private Vector3 targetPositionDoor, targetPositionTable;
     private int speed = 4;
 
@@ -29,6 +29,10 @@ public class MainCharacterScript : MonoBehaviour {
         {
             Move();
         }
+        if(isZooming != 0)
+        {
+            Zoom();
+        }
     }
 
     public void SetMove(int move)
@@ -45,7 +49,7 @@ public class MainCharacterScript : MonoBehaviour {
             if (transform.position == targetPositionDoor)
             {
                 isMoving = 0;
-                SceneManager.LoadScene("SmithyScene");
+                isZooming = 1;
             }
         }
         else if (isMoving == 2)
@@ -56,6 +60,13 @@ public class MainCharacterScript : MonoBehaviour {
                 isMoving = 0;
             }
         }
+    }
+
+    private void Zoom()
+    {
+        GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, 60, Time.deltaTime * 5);
+        SceneManager.LoadScene("SmithyScene");
+
     }
 
 }
