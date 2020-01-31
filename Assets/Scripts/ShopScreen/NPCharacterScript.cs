fb723880a131bb46d0cb5f50530078208c6ac7a3
Teodor G <grigor.teodor@gmail.com>
2020-01-31 23:34:42 +0000

    Update NPCharacterScript.cs

--- Assets/Scripts/ShopScreen/NPCharacterScript.cs
+++ Assets/Scripts/ShopScreen/NPCharacterScript.cs
@@ -7,6 +7,7 @@ public class NPCharacterScript : MonoBehaviour {
     private Vector3 targetPositionTable;
     private int speed = 4;
     private int isMoving = 0;
+    private TextScript textScript;
 
     // Use this for initialization
     void Start () {
@@ -20,13 +21,24 @@ public class NPCharacterScript : MonoBehaviour {
         targetPositionTable.x = table.transform.position.x - table.GetComponent<SpriteRenderer>().bounds.size.x / 2 - GetComponent<SpriteRenderer>().bounds.size.x / 2 - 0.2f;
         targetPositionTable.y = table.transform.position.y + table.GetComponent<SpriteRenderer>().bounds.size.y / 5;
         targetPositionTable.z = transform.position.z;
+
+        GameObject textHolder = GameObject.FindGameObjectWithTag("TextHolderShop");
+        textScript = (TextScript)textHolder.GetComponent(typeof(TextScript));
     }
 	
 	// Update is called once per frame
 	void Update () {
 		if(isMoving == 1)
         {
-            transform.position = Vector3.MoveTowards(transform.position, targetPositionTable, speed * Time.deltaTime);
+            if (transform.position == targetPositionTable)
+            {
+                isMoving = 0;
+                textScript.MakeVisible(true);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPositionTable, speed * Time.deltaTime);
+            }
         }
 	}
 
