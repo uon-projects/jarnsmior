e1e5d4c128dae365cbb03e816435523f86a4cd9c
Teodor G <grigor.teodor@gmail.com>
2020-01-31 20:19:00 +0000

    Update MainCharacterScript.cs

--- Assets/Scripts/ShopScreen/MainCharacterScript.cs
+++ Assets/Scripts/ShopScreen/MainCharacterScript.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainCharacterScript : MonoBehaviour {
 
     private bool isMoving = false;
-    private Vector3 targetPosition;
+    private Vector3 targetPositionDoor, targetPositionTable;
     private int speed = 4;
 
 	// Use this for initialization
 	void Start ()
     {
         GameObject door = GameObject.FindGameObjectWithTag("DoorShop");
-        targetPosition.x = door.transform.position.x;
-        targetPosition.y = door.transform.position.y - door.GetComponent<SpriteRenderer>().bounds.size.y / 2 + GetComponent<SpriteRenderer>().bounds.size.y / 2 - 0.2f;
-        targetPosition.z = transform.position.z;
+        targetPositionDoor.x = door.transform.position.x;
+        targetPositionDoor.y = door.transform.position.y - door.GetComponent<SpriteRenderer>().bounds.size.y / 2 + GetComponent<SpriteRenderer>().bounds.size.y / 2 - 0.2f;
+        targetPositionDoor.z = transform.position.z;
+
+        GameObject table = GameObject.FindGameObjectWithTag("TableShop");
+        targetPositionTable.x = table.transform.position.x + door.GetComponent<SpriteRenderer>().bounds.size.x / 2 - GetComponent<SpriteRenderer>().bounds.size.x / 2 + 0.2f;
+        targetPositionTable.y = table.transform.position.y;
+        targetPositionTable.z = transform.position.z;
     }
 	
 	// Update is called once per frame
@@ -33,10 +39,11 @@ public class MainCharacterScript : MonoBehaviour {
 
     public void Move()
     {
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-        if(transform.position == targetPosition)
+        transform.position = Vector3.MoveTowards(transform.position, targetPositionTable, speed * Time.deltaTime);
+        if(transform.position == targetPositionTable)
         {
             isMoving = false;
+            SceneManager.LoadScene("SmithyScene");
         }
     }
 
