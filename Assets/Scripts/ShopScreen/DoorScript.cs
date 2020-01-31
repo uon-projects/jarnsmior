028f1106f0ea3a5ce586ecb0c3f07e1c8a510d45
Teodor G <grigor.teodor@gmail.com>
2020-01-31 20:07:28 +0000

    Create DoorScript.cs

---
+++ Assets/Scripts/ShopScreen/DoorScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    public GameObject mainCharacter;
+    public MainCharacterScript mainCharacterScript;
+
+    // Use this for initialization
+    void Start ()
+    {
+        GameObject mainCharacter = GameObject.FindGameObjectWithTag("MainCharacterShop");
+        mainCharacterScript = (MainCharacterScript)mainCharacter.GetComponent(typeof(MainCharacterScript));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        if (mainCharacterScript != null)
+        {
+            mainCharacterScript.SetMove(true);
+        }
+    }
+
+}
