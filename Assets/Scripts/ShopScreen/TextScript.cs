2f12086496bebf922118075c7a317b87953211b3
Teodor G <grigor.teodor@gmail.com>
2020-01-31 23:34:48 +0000

    Update TextScript.cs

--- Assets/Scripts/ShopScreen/TextScript.cs
+++ Assets/Scripts/ShopScreen/TextScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
+[System.Serializable]
 public class TextChoice
 {
     //these variables are case sensitive and must match the strings "firstName" and "lastName" in the JSON.
@@ -13,6 +14,7 @@ public class TextChoice
     public bool choicePick;
 }
 
+[System.Serializable]
 public class TextChoices
 {
     public List<TextChoice> story_line;
@@ -20,22 +22,49 @@ public class TextChoices
 
 public class TextScript : MonoBehaviour {
 
-    public TextChoices shipTypes;
+    private TextChoices textValues;
+    int textToShow = 0;
+    public bool isVisible = false;
+    private TypeWritterEffect typeWritterEffect;
+
     // Use this for initialization
     void Start ()
     {
         string path = "Assets/JSON/story.json";
         string contents = File.ReadAllText(path);
-        shipTypes = JsonUtility.FromJson<TextChoices>(contents);
-        
-        for(int i=0; i<shipTypes.story_line.Capacity; i++)
+        textValues = JsonUtility.FromJson<TextChoices>(contents);
+
+        GameObject textShop = GameObject.FindGameObjectWithTag("TextShop");
+        typeWritterEffect = (TypeWritterEffect)textShop.GetComponent(typeof(TypeWritterEffect));
+
+        if (textValues != null)
         {
-            print(shipTypes.story_line[i].id);
+            for (int i = 0; i < textValues.story_line.Capacity; i++)
+            {
+                //print(textValues.story_line[i].id + ", " + textValues.story_line[i].text);
+            }
         }
+        setVisibility();
+    }
+
+    public void setVisibility()
+    {
+        GetComponent<Renderer>().enabled = isVisible;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(isVisible && !typeWritterEffect.startEffect)
+        {
+            typeWritterEffect.startEffect = true;
+            typeWritterEffect.fullText = textValues.story_line[textToShow].text;
+            print(textValues.story_line[textToShow].text);
+        }
 	}
+
+    public void MakeVisible(bool visibile)
+    {
+        isVisible = visibile;
+        setVisibility();
+    }
 }
