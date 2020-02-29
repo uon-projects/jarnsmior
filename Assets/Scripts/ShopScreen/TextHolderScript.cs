8486ae49c07edadf47c2a73b9db45c3ae32c98ca
Teodor G <grigor.teodor@gmail.com>
2020-02-29 14:11:11 +0000

    Create TextHolderScript.cs

---
+++ Assets/Scripts/ShopScreen/TextHolderScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TextHolderScript : MonoBehaviour {
+   
+
+    // Use this for initialization
+    void Start ()
+    {
+        float camHalfHeight = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().orthographicSize;
+        float screenAspect = (float)Screen.width / (float)Screen.height;
+        float camHalfWidth = screenAspect * camHalfHeight;
+        float camWidth = 2.0f * camHalfWidth;
+        Vector3 mPos = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        mPos.z = transform.position.z;
+        mPos.y = -camHalfWidth/2;
+        mPos.x = 0;
+        transform.position = mPos;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    
+}
